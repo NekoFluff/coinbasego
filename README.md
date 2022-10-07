3f7e4c3f58d0e35d1077df508a04fe17e48db7b9
Alex Nou <alexnou@gmail.com>
2022-10-06 23:43:04 -0700

    fix: fix accidental string replacement

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A client written in golang to work with the coinbasego exchange rest API.
 
-Coinbase pro reference docs: https://docs.cloud.coinbasego.com/exchange/reference
+Coinbase pro reference docs: https://docs.cloud.coinbase.com/exchange/reference
 
 **NOTE**: Does not implement every feature in the reference doc.
 
--- websocket.go
+++ websocket.go
@@ -60,7 +60,7 @@ type TickerMessage struct {
 }
 
 func (client *Client) WebSocketCreate(channels []Channel, wsh WebSockerHandler) *WebSocket {
-	socketUrl := "wss://ws-feed.exchange.coinbasego.com"
+	socketUrl := "wss://ws-feed.exchange.coinbase.com"
 	conn, _, err := websocket.DefaultDialer.Dial(socketUrl, nil)
 	if err != nil {
 		log.Fatal("Error connecting to Websocket Server:", err)
