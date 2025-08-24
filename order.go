da62d4a47a3ae139c16b9c7020b5a47eca21d7b8
NekoFluff <alexnou@gmail.com>
2025-08-24 15:20:20 -0700

    feat: update order structures and create order request

--- order.go
+++ order.go
@@ -14,66 +14,78 @@ type OrderResponse struct {
 	Order Order `json:"order"`
 }
 
+type LimitLimitGTD struct {
+	QuoteSize  string    `json:"quote_size,omitempty"`
+	BaseSize   string    `json:"base_size,omitempty"`
+	LimitPrice string    `json:"limit_price,omitempty"`
+	EndTime    time.Time `json:"end_time,omitempty"`
+	PostOnly   bool      `json:"post_only,omitempty"`
+}
+
+type LimitLimitGTC struct {
+	QuoteSize  string `json:"quote_size,omitempty"`
+	BaseSize   string `json:"base_size,omitempty"`
+	LimitPrice string `json:"limit_price,omitempty"`
+	PostOnly   bool   `json:"post_only,omitempty"`
+}
+
+type LimitLimitFOK struct {
+	QuoteSize  string `json:"quote_size,omitempty"`
+	BaseSize   string `json:"base_size,omitempty"`
+	LimitPrice string `json:"limit_price,omitempty"`
+}
+
+type SorLimitIOC struct {
+	QuoteSize  string `json:"quote_size,omitempty"`
+	BaseSize   string `json:"base_size,omitempty"`
+	LimitPrice string `json:"limit_price,omitempty"`
+}
+
+type MarketmarketIOC struct {
+	QuoteSize string `json:"quote_size,omitempty"`
+	BaseSize  string `json:"base_size,omitempty"`
+}
+
+type OrderConfiguration struct {
+	MarketmarketIOC *MarketmarketIOC `json:"marketmarket_ioc,omitempty"`
+	SorLimitIOC     *SorLimitIOC     `json:"sor_limit_ioc,omitempty"`
+	LimitLimitGTC   *LimitLimitGTC   `json:"limit_limit_gtc,omitempty"`
+	LimitLimitGTD   *LimitLimitGTD   `json:"limit_limit_gtd,omitempty"`
+	LimitLimitFOK   *LimitLimitFOK   `json:"limit_limit_fok,omitempty"`
+	// TODO: There are others
+}
+
 type Order struct {
-	OrderID            string `json:"order_id"`
-	ProductID          string `json:"product_id"`
-	UserID             string `json:"user_id"`
-	OrderConfiguration struct {
-		MarketmarketIOC struct {
-			QuoteSize string `json:"quote_size"`
-			BaseSize  string `json:"base_size"`
-		} `json:"marketmarket_ioc"`
-		SorLimitIOC struct {
-			QuoteSize  string `json:"quote_size"`
-			BaseSize   string `json:"base_size"`
-			LimitPrice string `json:"limit_price"`
-		} `json:"sor_limit_ioc"`
-		LimitLimitGTC struct {
-			QuoteSize  string `json:"quote_size"`
-			BaseSize   string `json:"base_size"`
-			LimitPrice string `json:"limit_price"`
-			PostOnly   bool   `json:"post_only"`
-		} `json:"limit_limit_gtc"`
-		LimitLimitGTD struct {
-			QuoteSize  string    `json:"quote_size"`
-			BaseSize   string    `json:"base_size"`
-			LimitPrice string    `json:"limit_price"`
-			EndTime    time.Time `json:"end_time"`
-			PostOnly   bool      `json:"post_only"`
-		} `json:"limit_limit_gtd"`
-		LimitLimitFOK struct {
-			QuoteSize  string `json:"quote_size"`
-			BaseSize   string `json:"base_size"`
-			LimitPrice string `json:"limit_price"`
-		} `json:"limit_limit_fok"`
-		// TODO: There are others
-	} `json:"order_configuration"`
-	Side                  OrderSide   `json:"side"`
-	ClientOrderID         string      `json:"client_order_id"`
-	Status                OrderStatus `json:"status"`
-	TimeInForce           TimeInForce `json:"time_in_force"`
-	CreatedTime           time.Time   `json:"created_time"`
-	CompletionPercentage  string      `json:"completion_percentage"`
-	FilledSize            string      `json:"filled_size"`
-	AverageFilledPrice    string      `json:"average_filled_price"`
-	NumberOfFills         string      `json:"number_of_fills"`
-	FilledValue           string      `json:"filled_value"`
-	PendingCancel         bool        `json:"pending_cancel"`
-	SizeInQuote           bool        `json:"size_in_quote"`
-	TotalFees             string      `json:"total_fees"`
-	SizeInclusiveOfFees   bool        `json:"size_inclusive_of_fees"`
-	TotalValueAfterFees   string      `json:"total_value_after_fees"`
-	TriggerStatus         string      `json:"trigger_status"` // TODO: This is an enum
-	OrderType             OrderType   `json:"order_type"`
-	RejectReason          string      `json:"reject_reason"` // TODO: This is an enum
-	Settled               bool        `json:"settled"`
-	ProductType           string      `json:"product_type"` // TODO: This is an enum
-	RejectMessage         string      `json:"reject_message"`
-	CancelMessage         string      `json:"cancel_message"`
-	OrderPlacementSource  string      `json:"order_placement_source"` // TODO: This is an enum
-	OutstandingHoldAmount string      `json:"outstanding_hold_amount"`
-	IsLiquidation         bool        `json:"is_liquidation"`
-	LastFillTime          time.Time   `json:"last_fill_time"`
+	OrderID               string             `json:"order_id"`
+	ProductID             string             `json:"product_id"`
+	UserID                string             `json:"user_id"`
+	OrderConfiguration    OrderConfiguration `json:"order_configuration"`
+	Side                  OrderSide          `json:"side"`
+	ClientOrderID         string             `json:"client_order_id"`
+	Status                OrderStatus        `json:"status"`
+	TimeInForce           TimeInForce        `json:"time_in_force"`
+	CreatedTime           time.Time          `json:"created_time"`
+	CompletionPercentage  string             `json:"completion_percentage"`
+	FilledSize            string             `json:"filled_size"`
+	AverageFilledPrice    string             `json:"average_filled_price"`
+	NumberOfFills         string             `json:"number_of_fills"`
+	FilledValue           string             `json:"filled_value"`
+	PendingCancel         bool               `json:"pending_cancel"`
+	SizeInQuote           bool               `json:"size_in_quote"`
+	TotalFees             string             `json:"total_fees"`
+	SizeInclusiveOfFees   bool               `json:"size_inclusive_of_fees"`
+	TotalValueAfterFees   string             `json:"total_value_after_fees"`
+	TriggerStatus         string             `json:"trigger_status"` // TODO: This is an enum
+	OrderType             OrderType          `json:"order_type"`
+	RejectReason          string             `json:"reject_reason"` // TODO: This is an enum
+	Settled               bool               `json:"settled"`
+	ProductType           string             `json:"product_type"` // TODO: This is an enum
+	RejectMessage         string             `json:"reject_message"`
+	CancelMessage         string             `json:"cancel_message"`
+	OrderPlacementSource  string             `json:"order_placement_source"` // TODO: This is an enum
+	OutstandingHoldAmount string             `json:"outstanding_hold_amount"`
+	IsLiquidation         bool               `json:"is_liquidation"`
+	LastFillTime          time.Time          `json:"last_fill_time"`
 	// TODO: Edit history
 	Leverage string `json:"leverage"`
 	// TODO: Attached Order (duplicate info of this Order struct)
@@ -174,24 +186,23 @@ const (
 	CancelAfterDay  = "day"
 )
 
-type OrderCreateBody struct {
-	ProfileID   string      `json:"profile_id,omitempty"`
-	Type        OrderType   `json:"type,omitempty"`
-	Side        OrderSide   `json:"side" binding:"required"`
-	ProductID   ProductID   `json:"product_id" binding:"required"`
-	STP         string      `json:"stp,omitempty"`
-	Stop        string      `json:"stop,omitempty"`
-	StopPrice   string      `json:"stop_price,omitempty"`
-	Price       string      `json:"price,omitempty"`
-	Size        string      `json:"size,omitempty"`
-	Funds       string      `json:"funds,omitempty"`
-	TimeInForce TimeInForce `json:"time_in_force,omitempty"`
-	CancelAfter CancelAfter `json:"cancel_after,omitempty"`
-	PostOnly    bool        `json:"post_only,omitempty"`
-	ClientOID   string      `json:"client_oid,omitempty"`
+// https://docs.cdp.coinbase.com/api-reference/advanced-trade-api/rest-api/orders/create-order
+type CreateOrderRequest struct {
+	// Required fields
+	ClientOrderID      string             `json:"client_order_id" binding:"required"`
+	ProductID          ProductID          `json:"product_id" binding:"required"`
+	Side               OrderSide          `json:"side" binding:"required"`
+	OrderConfiguration OrderConfiguration `json:"order_configuration" binding:"required"`
+
+	// Optional Fields
+	Leverage                   string             `json:"leverage,omitempty"`
+	MarginType                 string             `json:"margin_type,omitempty"` // Either "CROSS" or "ISOLATED"
+	PreviewID                  string             `json:"preview_id,omitempty"`
+	AttachedOrderConfiguration OrderConfiguration `json:"attached_order_configuration,omitempty"`
+	SmartOrderRoutingDisabled  bool               `json:"sor_disabled,omitempty"`
 }
 
-func (client *Client) OrderCreate(body OrderCreateBody) (*Order, error) {
+func (client *Client) CreateOrder(body CreateOrderRequest) (*Order, error) {
 	b, err := json.Marshal(body)
 	if err != nil {
 		return nil, err
@@ -200,7 +211,7 @@ func (client *Client) OrderCreate(body OrderCreateBody) (*Order, error) {
 	var order Order
 	req := Request{
 		Method:  "POST",
-		PathURL: "/orders",
+		PathURL: "/api/v3/brokerage/orders",
 		Body:    b,
 	}
 
